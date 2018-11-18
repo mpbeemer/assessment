ef0449c60b595e72ec7a30dfe60801f86135aca3
Michael Beemer <mpbeemer@gmail.com>
2018-11-18 18:08:19 -0500

    Initializing assessment materials

---
+++ assessment/src/com/routeone/interview/StoreRegister.java
@@ -0,0 +1,12 @@
+package com.routeone.interview;
+import java.io.File;
+import java.util.List;
+public class StoreRegister {
+ 
+    public void loadInventory(File inventoryFile){
+    }
+ 
+    public Receipt checkoutOrder(List<String> items) {
+        return null;
+    }
+}
